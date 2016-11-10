64901a57abde16ff8cbe61b2773564307b25c8d2
Mauj15 <mauj1_15@uni.worc.ac.uk>
2016-11-10 12:20:40 +0000

    Add files via upload

--- touchGestureCode/www/js/index.js
+++ touchGestureCode/www/js/index.js
@@ -7,12 +7,13 @@ $(document).on("pagecreate","#pageone",function(){
  	});                       
 
 	$('#taptext').on("tap",function(){
-    	alert("Stop tapping!");
+    	$(this).css('color', 'red');
  	}); 
 
 	$('#swipetext').on("swipeleft",function(){
     	$(this).css('color', 'green');
-  	});   
+  	})
+    ('#swiperight',function(){ $(this).css('color','blue');} )
 
 
 });
