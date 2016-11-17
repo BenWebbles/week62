a1de0d8c52c91ea0b26f5532606c41855d2204a0
BenWebbles <webb1_15@uni.worc.ac.uk>
2016-11-17 10:47:47 +0000

    poo

--- touchGestureCode/www/js/index.js
+++ touchGestureCode/www/js/index.js
@@ -2,18 +2,13 @@
 
 $(document).on("pagecreate","#pageone",function(){
   
-  	$('#tapholdtext').on("taphold",function(){
-    	$(this).hide();
- 	});                       
+
 
 	$('#taptext').on("tap",function(){
     	$(this).css('color', 'red');
  	}); 
 
-	$('#swipetext').on("swipeleft",function(){
-    	$(this).css('color', 'green');
-  	})
-  
+
 
 
 });
